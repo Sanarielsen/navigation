bc1dfc73017e2a577ae576efbb9593dae3054f8d
Sanarielsen <sanarielsen.dev@gmail.com>
2025-12-10 16:58:47 -0300

    feat(NVG-02): create stack navigation structure
    Create a stack navigation to show how can I navigate between different
    screens and following types of navigation with best way possible.

---
+++ src/app/Home.tsx
@@ -0,0 +1,22 @@
+import { View } from "react-native"
+
+import { StackRoutesProps } from "@/routes/StackRoutes"
+
+import { Header } from "@/components/Header"
+import { Title } from "@/components/Title"
+import { ButtonIcon } from "@/components/ButtonIcon"
+
+export function Home({ navigation }: StackRoutesProps<"home">) {
+
+  return (
+    <View style={{flex: 1, padding: 32, paddingTop: 54 }}>
+      <Header>
+        <Title>Home</Title>
+        <ButtonIcon 
+          name="add-circle" 
+          onPress={() => navigation.navigate("product", { id: "30" })}
+        />
+      </Header>
+    </View>
+  )
+}
